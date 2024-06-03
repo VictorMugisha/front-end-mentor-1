4b44231e177c45c704d3a55724dd2f1ec3792f11
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-06-03 16:35:14 +0200

    About to finish

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "custom-bg-200": "rgb(226,241,231)",
+        "custom-green-200": "rgb(12,125,105)",
+      },
+    },
   },
   plugins: [],
 }
